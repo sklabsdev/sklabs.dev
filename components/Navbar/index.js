86cd8d5de327da473be4855071fcbb0e8a0978e3
Jakub Sikora <j.sikora@livechatinc.com>
2023-02-03 11:12:15 +0100

    fix build

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @next/next/no-img-element */
 import { useState, useEffect, useRef, useMemo } from "react";
 import Link from "next/link";
